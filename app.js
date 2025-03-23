aeb9df748b671529dc645cfc3a3e8ad360c458f4
劉泓昇 <liuhongsheng@liuhongshengdeMacBook-Air.local>
2025-03-23 15:36:45 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
+require('dotenv').config(); // 確保環境變量被加載
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.connect(process.env.MONGODB_URI)
+
+
+// if(process.env.Node_ENV !=='production'){
+//     require('dotenv').config()
+// }
+
+const db = mongoose.connection
+db.on('error',() => console.log('MongoDB error'))
+db.once('open', () => console.log('MongoDB success'))
+
 
 app.get('/', (req,res)=>{
     res.send('Hello World')
