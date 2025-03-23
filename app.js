6347de1d364b69c2fb6dd7d6e73c2664520fc8e6
劉泓昇 <liuhongsheng@liuhongshengdeMacBook-Air.local>
2025-03-23 19:26:15 +0800

    feat: add create todo feature

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config(); // 確保環境變量被加載
 const express = require('express')
 const mongoose = require('mongoose')
 const { engine } = require('express-handlebars');
+const bodyParser = require('body-parser')
 
 const app = express()
 
@@ -21,6 +22,8 @@ app.engine('.hbs', engine({ extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.set('views', './views');
 
+app.use(bodyParser.urlencoded({extends: true}))
+
 
 app.get('/', (req,res)=>{
     Todo.find()
@@ -29,6 +32,17 @@ app.get('/', (req,res)=>{
             todos => res.render('index',{todos})
         )
         .catch(error=> console.error(error))
+})
+
+app.get('/new', (req, res) => {
+    return res.render('new')  
+})
+
+app.post('/todos', (req, res) =>{
+    const name = req.body.name
+    return Todo.create({name}).then(()=>{
+        res.redirect('/')
+    }).catch(error => console.error(error))
 })
 
 app.listen(3000, ()=>{
