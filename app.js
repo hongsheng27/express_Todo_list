b800c5cd5c240f0623de7483f0965894141165ee
劉泓昇 <liuhongsheng@liuhongshengdeMacBook-Air.local>
2025-03-24 13:28:05 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const methodOverride = require('method-override')
 
 const app = express()
 
-const Todo = require('./models/todo')
+const routes = require('./routes')
 
 mongoose.connect(process.env.MONGODB_URI)
 
@@ -24,73 +24,8 @@ app.use(bodyParser.urlencoded({extends: true}))
 app.use(methodOverride('_method'))
 
 
-app.get('/', (req,res)=>{
-    Todo.find()
-        .lean()
-        .sort({_id: 'asc'})
-        .then(
-            todos => res.render('index',{todos})
-        )
-        .catch(error=> console.error(error))
-})
+app.use(routes)
 
-app.get('/new', (req, res) => {
-    return res.render('new')  
-})
-
-app.post('/todos', (req, res) =>{
-    const name = req.body.name
-    return Todo.create({name}).then(()=>{
-        res.redirect('/')
-    }).catch(error => console.error(error))
-})
-
-app.get('/todos/:id', (req,res)=>{
-    const id = req.params.id
-    return Todo.findById(id)
-        .lean()
-        .then((todo) => res.render('detail', {todo}))
-        .catch(error => console.error(error))
-})
-
-app.get('/todos/:id/edit', (req,res)=>{
-    const id = req.params.id
-    return Todo.findById(id)
-        .lean()
-        .then((todo) => res.render('edit', {todo}))
-        .catch(error => console.error(error))
-})
-
-app.put('/todos/:id', (req,res)=>{
-    const id = req.params.id
-    const {name, isDone} = req.body
-    return Todo.findById(id)
-        .then(todo => {
-            todo.name = name
-            todo.isDone = isDone === 'on'
-            return todo.save()
-        })
-        .then(()=> res.redirect(`/todos/${id}`))
-        .catch(error => console.error(error))
-})
-
-
-app.delete('/todos/:id', (req,res)=>{
-    const id = req.params.id
-    return Todo.findById(id)
-        .then(todo => {
-            if(!todo){
-                throw new Error('todo not found')
-            }
-            return todo.deleteOne()
-        })
-        .then(()=> res.redirect(`/`))
-        .catch(error => console.error(error))
-    // return Todo.findById(id)
-    //     .then(todo => todo.remove())
-    //     .then(()=> res.redirect(`/`))
-    //     .catch(error => console.error(error))
-})
 
 app.listen(3000, ()=>{
     console.log('Server is running on port http:localhost3000')
