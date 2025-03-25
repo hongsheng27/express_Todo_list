cbd00820e461d63e52c9462e53d72f028ec41d66
劉泓昇 <liuhongsheng@liuhongshengdeMacBook-Air.local>
2025-03-25 16:05:58 +0800

    feat: add register feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -9,5 +9,24 @@ router.post('/login', (req, res) => {});
 router.get('/register', (req, res) => {
   res.render('register');
 });
-
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body;
+  User.findOne({ email })
+    .then((user) => {
+      if (user) {
+        console.log('User already exist!');
+        return res.render('register', {
+          name,
+          email,
+          password,
+          confirmPassword,
+        });
+      } else {
+        return User.create({ email, password, name })
+          .then(() => res.redirect('/'))
+          .catch((err) => console.log(err));
+      }
+    })
+    .catch((err) => console.log(err));
+});
 module.exports = router;
