4268448a5fe73fe7573edee9b140ea5da781cc11
劉泓昇 <liuhongsheng@liuhongshengdeMacBook-Air.local>
2025-03-27 17:37:34 +0800

    feat: add relation between Users and Todos Model

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -7,8 +7,9 @@ router.get('/new', (req, res) => {
 });
 
 router.post('/', (req, res) => {
+  const userId = req.user._id;
   const name = req.body.name;
-  return Todo.create({ name })
+  return Todo.create({ name, userId })
     .then(() => {
       res.redirect('/');
     })
@@ -16,37 +17,43 @@ router.post('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  const id = req.params.id;
-  return Todo.findById(id)
+  const userId = req.user._id;
+  const _id = req.params.id;
+  return Todo.findOne({ _id, userId })
     .lean()
-    .then((todo) => res.render('detail', { todo }))
+    .then((todo) => {
+      res.render('detail', { todo });
+    })
     .catch((error) => console.error(error));
 });
 
 router.get('/:id/edit', (req, res) => {
-  const id = req.params.id;
-  return Todo.findById(id)
+  const userId = req.user._id;
+  const _id = req.params.id;
+  return Todo.findOne({ _id, userId })
     .lean()
     .then((todo) => res.render('edit', { todo }))
     .catch((error) => console.error(error));
 });
 
 router.put('/:id', (req, res) => {
-  const id = req.params.id;
+  const _id = req.params.id;
+  const userId = req.user._id;
   const { name, isDone } = req.body;
-  return Todo.findById(id)
+  return Todo.findOne({ _id, userId })
     .then((todo) => {
       todo.name = name;
       todo.isDone = isDone === 'on';
       return todo.save();
     })
-    .then(() => res.redirect(`/todos/${id}`))
+    .then(() => res.redirect(`/todos/${_id}`))
     .catch((error) => console.error(error));
 });
 
 router.delete('/:id', (req, res) => {
-  const id = req.params.id;
-  return Todo.findById(id)
+  const _id = req.params.id;
+  const userId = req.user._id;
+  return Todo.findOne({ _id, userId })
     .then((todo) => {
       if (!todo) {
         throw new Error('todo not found');
