431404333eec67389220d1fb1d22e033a1dc15cc
劉泓昇 <liuhongsheng@liuhongshengdeMacBook-Air.local>
2025-03-28 21:38:36 +0800

    refactor: generate seed data with root user

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,11 +1,42 @@
 require('dotenv').config();
 const db = require('../../config/mongoose');
-
+const bcrypt = require('bcryptjs');
+const User = require('../user');
 const Todo = require('../todo');
 
+const SEED_USER = {
+  name: 'root',
+  email: 'root@example.com',
+  password: '12345678',
+};
+
 db.once('open', () => {
-  for (let i = 0; i < 10; i++) {
-    Todo.create({ name: `name-${i}` });
-  }
-  console.log('done');
+  bcrypt
+    .genSalt(10)
+    .then((salt) => bcrypt.hash(SEED_USER.password, salt))
+    .then((hash) =>
+      User.create({
+        name: SEED_USER.name,
+        email: SEED_USER.email,
+        password: hash,
+      })
+    )
+    .then((user) => {
+      const userId = user._id;
+      return Promise.all(
+        Array.from({ length: 10 }, (_, i) => {
+          return Todo.create({
+            name: `name-${i}`,
+            userId,
+          });
+        })
+      );
+    })
+    .then(() => {
+      console.log('done');
+      process.exit();
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 });
